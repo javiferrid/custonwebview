cfbb1c3368b01c280332760258c8fa37fb430c51
jferrid <javier.ferri-dedios@whitekongsoftware.com>
2023-05-02 09:20:46 +0200

    Initial Commit

---
+++ WebViewConsumer/WebViewConsumerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewConsumerApp.swift
+//  WebViewConsumer
+//
+//  Created by Javier Ferri de Dios on 2/5/23.
+//
+
+import SwiftUI
+
+@main
+struct WebViewConsumerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
